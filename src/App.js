a47a4edfa105173c26fe5a75612707397743510d
CherrylCao <caotranhonganh251299@gmail.com>
2024-01-08 13:01:32 +0700

    lần 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import UserTemplate from "./template/userTemplate";
+import Home from "./pages/Home/home";
 
 function App() {
   return (
     <>
       <Routes>
-
+        <Route path="/" element={<UserTemplate />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
