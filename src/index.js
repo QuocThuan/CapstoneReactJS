6e9095fb8bfac49fff4c130154e367f03ce04d72
thnkieu <thienkieu611@gmail.com>
2024-01-18 02:36:09 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./App";
+import "./assets/sass/index.scss";
 
 // react-router-dom
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // redux toolkit
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route index element={<Home />}></Route>
+      </Routes>
     </BrowserRouter>
   </Provider>
 );
